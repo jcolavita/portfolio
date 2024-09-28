cb394118c231b536789ebad261ef56df013bc297
Colavita <joseacp57@gmail.com>
2024-09-28 13:57:25 -0400

    Add justify property to footer buttons

--- src/app/sections/footerSection.jsx
+++ src/app/sections/footerSection.jsx
@@ -12,13 +12,13 @@ export default function footerSection() {
       </div>
       <div className="w-[2px] h-auto bg-white ml-5 mr-5"></div>
       <div className="flex flex-col  h-auto md:flex-row w-full md:justify-center items-start md:gap-12 gap-2 md:-mt-4">
-        <button className=" text-sm flex items-center gap-5">
+        <button className=" text-sm flex items-center justify-between w-full ">
           Inicio <IoMdHome className="block md:hidden" />
         </button>
-        <button className=" text-sm flex items-center gap-5">
+        <button className=" text-sm flex items-center justify-between w-full ">
           Servicios <FaLaptopCode className="block md:hidden" />
         </button>
-        <button className=" text-sm flex items-center gap-5">
+        <button className=" text-sm flex items-center justify-between w-full ">
           Contacto <MdMessage className="block md:hidden" />
         </button>
       </div>
