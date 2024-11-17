619241d72d52f56e08a50eba1e7542f01022c775
Colavita <joseacp57@gmail.com>
2024-11-17 13:39:03 -0400

    fix services "aplicacion escritorio" text title

--- src/app/components/sections/servicesSection/services.jsx
+++ src/app/components/sections/servicesSection/services.jsx
@@ -21,18 +21,21 @@ export default function services() {
         <div className=" group md:w-[40%] w-[45%] bg-white relative rounded-xl flex overflow-hidden hover:scale-105 transition-all ease-in-out duration-200 ">
           <div className="w-full md:h-0 h-full absolute bg-black/70 md:group-hover:h-full transition-all ease-in-out duration-300"></div>
           <Image src={appEscritorio} alt="Aps de escritorio " />
-
-          <h3 className=" md:group-hover:opacity-100 md:opacity-0 opacity-100 transition-all ease-in-out duration-300  md:w-[350px] w-auto md:h-10 h-20 absolute left-0 right-0 bottom-0 top-0 m-auto text-center font-semibold md:text-4xl text-xl  ">
-            APLICACION DE ESCRITORIO
-          </h3>
+          <div className="flex items-center justify-center absolute left-0 top-0 right-0 bottom-0 m-auto ">
+            <h3 className=" md:group-hover:opacity-100 md:opacity-0 opacity-100 transition-all ease-in-out duration-300  text-center font-semibold md:text-4xl text-xl  ">
+              APLICACION ESCRITORIO
+            </h3>
+          </div>
         </div>
         <div className=" group md:w-[40%] w-[45%] md:hover:scale-105 transition-all ease-in-out duration-200 bg-white relative rounded-xl flex overflow-hidden ">
           <div className="w-full md:h-0 h-full absolute bg-black/70 md:group-hover:h-full transition-all ease-in-out duration-300"></div>
           <Image src={appWeb} alt="Aps de escritorio " />
 
-          <h3 className=" md:group-hover:opacity-100 md:opacity-0 opacity-100 transition-all ease-in-out duration-300  md:w-[300px] w-auto h-10 absolute left-0 right-0 bottom-0 top-0 m-auto text-center font-semibold md:text-4xl text-xl  ">
-            APLICACION WEB
-          </h3>
+          <div className="flex items-center justify-center absolute left-0 top-0 right-0 bottom-0 m-auto ">
+            <h3 className=" md:group-hover:opacity-100 md:opacity-0 opacity-100 transition-all ease-in-out duration-300  text-center  font-semibold md:text-4xl text-xl  ">
+              APLICACION WEB
+            </h3>
+          </div>
         </div>
       </div>
     </section>
