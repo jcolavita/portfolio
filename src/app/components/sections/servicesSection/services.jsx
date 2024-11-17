526e5891df33ce2aa0123286939459fceaeac043
Colavita <joseacp57@gmail.com>
2024-11-17 14:17:33 -0400

    change service cards disposition

--- src/app/components/sections/servicesSection/services.jsx
+++ src/app/components/sections/servicesSection/services.jsx
@@ -17,17 +17,17 @@ export default function services() {
         Codificamos tus sueños en realidad. ¡Descubre el poder de la tecnología
         con nosotros!
       </p>
-      <div className=" flex justify-around">
-        <div className="shadow-md shadow-black group md:w-[40%] w-[45%] bg-white relative rounded-xl flex overflow-hidden hover:scale-105 transition-all ease-in-out duration-200 ">
+      <div className=" flex md:flex-row flex-col justify-around gap-y-5">
+        <div className="shadow-md shadow-black group md:w-[40%] w-[100%] bg-white relative rounded-xl flex overflow-hidden hover:scale-105 transition-all ease-in-out duration-200 ">
           <div className="w-full md:h-0 h-full absolute bg-black/70 md:group-hover:h-full transition-all ease-in-out duration-300"></div>
           <Image src={appEscritorio} alt="Aps de escritorio " />
           <div className="flex items-center justify-center absolute left-0 top-0 right-0 bottom-0 m-auto ">
             <h3 className=" md:group-hover:opacity-100 md:opacity-0 opacity-100 transition-all ease-in-out duration-300  text-center font-semibold md:text-4xl text-xl  ">
-              APLICACION ESCRITORIO
+              APLICACION DE ESCRITORIO
             </h3>
           </div>
         </div>
-        <div className="shadow-md shadow-black group md:w-[40%] w-[45%] md:hover:scale-105 transition-all ease-in-out duration-200 bg-white relative rounded-xl flex overflow-hidden ">
+        <div className="shadow-md shadow-black group md:w-[40%] w-[100%] md:hover:scale-105 transition-all ease-in-out duration-200 bg-white relative rounded-xl flex overflow-hidden ">
           <div className="w-full md:h-0 h-full absolute bg-black/70 md:group-hover:h-full transition-all ease-in-out duration-300"></div>
           <Image src={appWeb} alt="Aps de escritorio " />
 
